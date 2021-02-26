61125a2b32aaf8866180d4fa77889d274b61bbb8
LCN-Ubuntu <18252084368@163.com>
2021-02-26 22:22:39 +0000

    V3.1

--- OpenCV-Color/colorList.py
+++ OpenCV-Color/colorList.py
@@ -8,7 +8,7 @@ import collections
 def getColorList():
     dict = collections.defaultdict(list)
  
-    # 黑色
+    # Black
     lower_black = np.array([0, 0, 0])
     upper_black = np.array([180, 255, 46])
     color_list = []
@@ -16,15 +16,15 @@ def getColorList():
     color_list.append(upper_black)
     dict['black'] = color_list
  
-    # #灰色
-    # lower_gray = np.array([0, 0, 46])
-    # upper_gray = np.array([180, 43, 220])
-    # color_list = []
-    # color_list.append(lower_gray)
-    # color_list.append(upper_gray)
-    # dict['gray']=color_list
+    # Gray
+    lower_gray = np.array([0, 0, 46])
+    upper_gray = np.array([180, 43, 220])
+    color_list = []
+    color_list.append(lower_gray)
+    color_list.append(upper_gray)
+    dict['gray']=color_list
  
-    # 白色
+    # White
     lower_white = np.array([0, 0, 221])
     upper_white = np.array([180, 30, 255])
     color_list = []
@@ -32,7 +32,7 @@ def getColorList():
     color_list.append(upper_white)
     dict['white'] = color_list
  
-    #红色
+    # Red1
     lower_red = np.array([156, 43, 46])
     upper_red = np.array([180, 255, 255])
     color_list = []
@@ -40,7 +40,7 @@ def getColorList():
     color_list.append(upper_red)
     dict['red']=color_list
  
-    # 红色2
+    # Red2
     lower_red = np.array([0, 43, 46])
     upper_red = np.array([10, 255, 255])
     color_list = []
@@ -48,7 +48,7 @@ def getColorList():
     color_list.append(upper_red)
     dict['red2'] = color_list
  
-    #橙色
+    # Orange
     lower_orange = np.array([11, 43, 46])
     upper_orange = np.array([25, 255, 255])
     color_list = []
@@ -56,7 +56,7 @@ def getColorList():
     color_list.append(upper_orange)
     dict['orange'] = color_list
  
-    #黄色
+    # Yellow
     lower_yellow = np.array([26, 43, 46])
     upper_yellow = np.array([34, 255, 255])
     color_list = []
@@ -64,7 +64,7 @@ def getColorList():
     color_list.append(upper_yellow)
     dict['yellow'] = color_list
  
-    #绿色
+    # Green
     lower_green = np.array([35, 43, 46])
     upper_green = np.array([77, 255, 255])
     color_list = []
@@ -72,7 +72,7 @@ def getColorList():
     color_list.append(upper_green)
     dict['green'] = color_list
  
-    #青色
+    # Cyan
     lower_cyan = np.array([78, 43, 46])
     upper_cyan = np.array([99, 255, 255])
     color_list = []
@@ -80,7 +80,7 @@ def getColorList():
     color_list.append(upper_cyan)
     dict['cyan'] = color_list
  
-    #蓝色
+    # Blue
     lower_blue = np.array([100, 43, 46])
     upper_blue = np.array([124, 255, 255])
     color_list = []
@@ -88,7 +88,7 @@ def getColorList():
     color_list.append(upper_blue)
     dict['blue'] = color_list
  
-    # 紫色
+    # Purple
     lower_purple = np.array([125, 43, 46])
     upper_purple = np.array([155, 255, 255])
     color_list = []
